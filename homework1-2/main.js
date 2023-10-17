c7f52dbec17b12b6738d83fac388692d0667bd72
aselker1 <mamatovaaselya28@gmail.com>
2023-10-17 19:40:55 +0600

    first commit

---
+++ homework1-2/main.js
@@ -0,0 +1,30 @@
+// const task = (number) => {
+// if (number === 1) {
+//     return 1;
+// } 
+
+// const next = task (number -1);
+// return number + next;
+// };
+
+// const result = task(5);
+// console.log(result); 
+
+
+// const task2 =(a,b) => {
+
+// }
+
+const task = (number) => {
+     if (number === 1) {
+         return 1;
+     } 
+    
+     const next = task (a,b,c);
+     return a+b+c;
+     };
+    
+     const result = task(1,7,9);
+     console.log(result); 
+    
+    
